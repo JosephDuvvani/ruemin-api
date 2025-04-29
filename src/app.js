a7b5d2f9422877bffbd07a1dbfce7188dc92407c
JosephDuvvani <josephduvvani@gmail.com>
2025-04-29 15:45:08 +0200

    Setup the express application

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import routes from "./routes/index.js";
+import { configDotenv } from "dotenv";
+
+configDotenv();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", routes.auth);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, console.log(`Now listening on port: ${PORT}`));
