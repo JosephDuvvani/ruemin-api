3349f9c8e3ea5345ae251a3802dd4c03191aeed8
JosephDuvvani <josephduvvani@gmail.com>
2025-05-12 19:29:02 +0200

    Upload & get new url with uploadPicture function

--- src/controllers/profile.js
+++ src/controllers/profile.js
@@ -1,5 +1,6 @@
 import models from "../models/index.js";
 import { body, validationResult } from "express-validator";
+import { upload, uploadPicture } from "../storage/storage.js";
 
 const validateNames = [
   body("firstname")
@@ -85,23 +86,30 @@ const updateBio = async (req, res) => {
   }
 };
 
-const updatePicture = async (req, res) => {
-  const { profileId } = req.params;
-  const { imageUrl } = req.body;
+const updatePicture = [
+  upload.single("file"),
+  async (req, res) => {
+    const { profileId } = req.params;
+    const file = req.file;
 
-  try {
-    const profile = await models.Profile.updatePicture(profileId, imageUrl);
-    const updated = {
-      imageUrl: profile.imageUrl,
-    };
+    if (!file) {
+      return res.status(400).send("No file uploaded.");
+    }
 
-    return res.json({ updated });
-  } catch (err) {
-    return res.status(500).json({
-      message: "Error updating profile picture",
-      error: err,
-    });
-  }
-};
+    try {
+      const url = await uploadPicture(file, profileId);
+      const profile = await models.Profile.updatePicture(profileId, url);
+      const updated = {
+        imageUrl: profile.imageUrl,
+      };
+      return res.json({ updated });
+    } catch (err) {
+      return res.status(500).json({
+        message: "Error updating profile picture",
+        error: err,
+      });
+    }
+  },
+];
 
 export { updateName, updateBio, updatePicture };
